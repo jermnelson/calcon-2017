0254e8e3f08f446ba1d03eaa37113bb52ef53523
Jeremy Nelson <jermnelson@gmail.com>
2017-08-09 17:17:11 -0600

    added support for furtive.css

--- views.py
+++ views.py
@@ -3,6 +3,12 @@ __author__ = "Jeremy Nelson"
 from flask import render_template, request
 from app import app
 
+@app.route("/background")
+def background():
+    return render_template("calcon-2017/content.html",
+        title="Background on Project")
+        
+
 @app.route("/")
 def home():
     return render_template("calcon-2017/index.html")
