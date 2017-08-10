bd20305194b6bd9dc925d9dae4b1c71bbcc05b8f
Jeremy Nelson <jermnelson@gmail.com>
2017-08-10 11:04:41 -0600

    added stub pages to presentation

--- views.py
+++ views.py
@@ -3,10 +3,11 @@ __author__ = "Jeremy Nelson"
 from flask import render_template, request
 from app import app
 
-@app.route("/background")
-def background():
-    return render_template("calcon-2017/background.html")
+@app.route("/<path:name>")
+def page(name):
+    return render_template("calcon-2017/{}.html".format(name))
         
+
 
 @app.route("/")
 def home():
